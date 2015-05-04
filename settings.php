29bfa59135a337f399da66b3856ae469d2d4b673
pantobp <nemanjapantovic@live.ac.me>
2015-05-04 14:44:58 +0200

    First commit

---
+++ settings.php
@@ -0,0 +1,8 @@
+<?php
+	$adresa="Stuart Levy DDS 9608 Kenwood Rd Cincinnati, Ohio 45242"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad="";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica=""; // AKo ne znate skracenicu ostavite praznu
+	$telephone=5136795094;// Broj koji se poziva na dugme CALL US
+	$theme="b";//Thema koja se aplicira na sve
+	$transitionefect="flip"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup"
+?>
